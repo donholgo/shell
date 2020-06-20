7bc21893d022d9cb78143485698cf813092979ec
Holger Wahlen <Tarquin@web.de>
2020-06-20 18:27:56 +0200

    Initial.

---
+++ path-functions.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# return 0 if PATH contains $1, 1 otherwise
+path_contains() {
+  case ":$PATH:" in
+    *":$1:"*) return 0
+      ;;
+  esac
+  return 1
+}
+
+path_remove() {
+  for dir; do
+    PATH=":$PATH:"
+    PATH="${PATH//:$dir:/:}"
+    PATH="${PATH#:}"
+    PATH="${PATH%:}"
+  done
+}
+
+path_append() {
+  for dir; do
+    path_remove "$dir"
+    PATH=$PATH:"$dir"
+  done
+}
+
+path_append_unless_present() {
+  for dir; do
+    path_contains "$dir" || path_append "$dir"
+  done
+}
+
+path_prepend() {
+  for dir; do
+    path_remove "$dir"
+    PATH="$dir":$PATH
+  done
+}
+
+path_prepend_unless_present() {
+  for dir; do
+    path_contains "$dir" || path_prepend "$dir"
+  done
+}
+
