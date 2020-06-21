b3647eb6b9dc124d979138f3b0d19d2415a1baa7
Holger Wahlen <Tarquin@web.de>
2020-06-21 16:45:29 +0200

    Removed extra blank.

--- prompt.sh
+++ prompt.sh
@@ -6,7 +6,7 @@ c1="${pcolor_bold}${pcolor_fgBlue}"
 c2="$pcolor_fgRed"
 c3="$pcolor_fgGreen"
 PS1="${c1}\u${c0}${c2}@${c0}${c1}\h${c0} ${c2}\W${c0}${c3}"'$(__git_ps1)'"${c0} ${c1}\$${pcolor_reset} "
-PS2="${c1}> ${pcolor_reset} "
+PS2="${c1}>${pcolor_reset} "
 unset c0 c1 c2 c3
 
 # variables to configure __git_ps1 (in PS1)
