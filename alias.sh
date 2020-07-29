156e83d114e3f3e799a507259370947face33b51
Holger Wahlen <Tarquin@web.de>
2020-07-29 18:25:38 +0200

    Turned 'vir' into function to allow call without arguments.

--- alias.sh
+++ alias.sh
@@ -7,7 +7,6 @@ alias df.='df -H .'
 alias ll='ls -l'
 alias md='mkdir'
 alias vip='vi -p'
-alias vir='vi -R -'
 
 mcd() {
   if [[ $# -eq 1 ]]; then
@@ -35,6 +34,15 @@ up() {
   done
   [[ -n $dir ]] && cd "$dir"
 }
+
+vir() {
+  if [[ $# -eq 0 ]]; then
+    vi -R -
+  else
+    vi -Rp "$@"
+  fi
+}
+
 
 xo() {
   for file; do
